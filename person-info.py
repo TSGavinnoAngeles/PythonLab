747b2af9361dc5c9f43a25ac0bc171d7a594c0f7
OthelloA <154335330+TSGavinnoAngeles@users.noreply.github.com>
2024-09-15 12:11:26 +0000

    Files contain the code for the Python Lab

---
+++ person-info.py
@@ -0,0 +1,5 @@
+fName =input('What is your first name? ')
+lName = input('What is your last name? ')
+loc = input('What is your location? ')
+age = input('What is our age? ')
+print("Hi "+fName + " "+ lName + " ! " + "Your location is "+ loc+" and you are " +age+" years old.")
